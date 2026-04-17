a1c3da170bc2d34d9b6652ea2e70a54fe3d66b12
moon <swer0101@gmail.com>
2026-04-17 16:03:19 +0900

    페이지 연결

--- js/oneGrove_sub4.js
+++ js/oneGrove_sub4.js
@@ -1,12 +1,11 @@
 //탭 메뉴
-$('.tab-area>li').on('click', function (e) {
-  e.preventDefault()
-  let filter = $(this).data('filter');
+$(".tab-area>li").on("click", function (e) {
+  e.preventDefault();
+  let filter = $(this).data("filter");
   //li강조 줄
-  $('.tab-area>li').removeClass('on');
-  $(this).addClass('on')
+  $(".tab-area>li").removeClass("on");
+  $(this).addClass("on");
   //탭 선택
-  $('.transport').removeClass('show')
-  $('.' + filter).addClass('show')
-
-})
+  $(".transport").removeClass("show");
+  $("." + filter).addClass("show");
+});
